0384f9e66cfe14a80e718f76571a18146625b71e
Ben Allred <skuttleman@gmail.com>
2017-12-01 17:52:17 -0700

    Day 4 Step 2

--- src/advent_2016/day_4.clj
+++ src/advent_2016/day_4.clj
@@ -10,7 +10,8 @@
 
 (defn ^:private s->room [line]
     (let [[_ letters sector-id checksum] (re-find (re-matcher #"(\D+)(\d+)\[(.*)\]" line))]
-        {:letters (s->letters (s/replace letters #"-" ""))
+        {:encrypted-name letters
+         :letters (s->letters (s/replace letters #"-" ""))
          :sector-id (Integer/parseInt sector-id)
          :checksum (seq checksum)}))
 
@@ -22,10 +23,36 @@
         (vec)
         (= checksum)))
 
-;; 158835
-(defn step-1 []
+(defn ^:private rotate-letter [times letter]
+    (if (= \- letter)
+        " "
+        (->> u/letter-seq
+            (drop-while (partial not= letter))
+            (drop times)
+            (first))))
+
+(defn ^:private room->name [{:keys [encrypted-name sector-id] :as room}]
+    (->> encrypted-name
+        (map rotate-letter (repeat (mod sector-id 26)))
+        (apply str)
+        (s/trim)
+        (assoc room :name)))
+
+(defn ^:private prep []
     (->> (u/read-file 4 #"\n")
         (map s->room)
-        (filter real-room?)
+        (filter real-room?)))
+
+;; 158835
+(defn step-1 []
+    (->> (prep)
         (map :sector-id)
         (reduce +)))
+
+;; 993
+(defn step-2 []
+    (->> (prep)
+        (map room->name)
+        (filter (comp (partial = "northpole object storage") :name))
+        (first)
+        (:sector-id)))
--- src/advent_2016/utils/core.clj
+++ src/advent_2016/utils/core.clj
@@ -1,6 +1,8 @@
 (ns advent-2016.utils.core
     (:require [clojure.string :as s]))
 
+(def letter-seq (iterate (fn [l] (if (= l \z) \a (char (inc (int l))))) \a))
+
 (defn split-n-trim [re input]
     (->> (s/split input re)
         (map s/trim)
